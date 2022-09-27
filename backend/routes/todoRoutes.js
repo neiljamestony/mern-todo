b48d77d65a59e4e47cddc27eed5043cfbf0dfae6
Perdigon <neil.james.perdigon@accenture.com>
2022-09-27 20:12:38 +0800

    update todo routes

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { getTodo, setTodo, updateTodo, removeTodo } = require('../controllers/todoController')
 //SHORTEN VIEW
-router.route('/').get(getTodo).post(setTodo)
-router.route('/:id').delete(removeTodo).put(updateTodo)
+// router.route('/').get(getTodo).post(setTodo)
+// router.route('/:id').delete(removeTodo).put(updateTodo)
 
-// router.get('/', getTodo)
-// router.post('/', setTodo)
-// router.put('/:id', updateTodo)
-// router.delete('/:id', removeTodo)
+router.get('/', getTodo)
+router.post('/', setTodo)
+router.put('/:id', updateTodo)
+router.delete('/:id', removeTodo)
 
 module.exports = router
