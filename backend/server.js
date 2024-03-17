9b2c9f6f25a0bc4597a2d009cc183b6a37280cdd
Perdigon <neil.james.perdigon@accenture.com>
2024-03-18 01:07:40 +0800

    allow all sites for accessing

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 3000;
-const cors = require("cors");
-const connectDB = require("./config/db");
+const cors = require('cors');
+const connectDB = require('./config/db');
 
 connectDB();
 const app = express();
 
 const config = {
-  origin:
-    process.env.NODE_ENV === "development"
-      ? process.env.LOCAL_HOST
-      : process.env.PROD_HOST,
+  origin: '*',
   credential: true,
   optionSuccessStatus: 200,
 };
@@ -21,7 +18,7 @@ app.use(cors(config));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users", require("./routes/UserRoutes"));
-app.use("/api/todos", require("./routes/todoRoutes"));
+app.use('/api/users', require('./routes/UserRoutes'));
+app.use('/api/todos', require('./routes/todoRoutes'));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
