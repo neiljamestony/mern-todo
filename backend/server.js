37171e9147379ee20f0f674abb34f5230e5658fa
Perdigon <neil.james.perdigon@accenture.com>
2024-03-18 01:14:33 +0800

    allow all sites for accessing

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,7 @@ const connectDB = require('./config/db');
 connectDB();
 const app = express();
 
-const config = {
-  origin: '*',
-};
-
-app.use(cors(config));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
